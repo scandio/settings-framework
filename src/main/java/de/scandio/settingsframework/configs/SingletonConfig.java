d57f1d99eefca5941053186452b72c972c2e6fa0
Georg Schmidl <(none)>
2018-05-24 12:02:49 +0200

    Remove Config interface

--- src/main/java/de/scandio/settingsframework/configs/SingletonConfig.java
+++ src/main/java/de/scandio/settingsframework/configs/SingletonConfig.java
@@ -1,59 +1,14 @@
 package de.scandio.settingsframework.configs;
 
 import de.scandio.settingsframework.settings.Config;
-import de.scandio.settingsframework.settings.Migrator;
 
-import java.util.List;
-import java.util.Map;
+public class SingletonConfig {
 
-public class SingletonConfig implements Config {
+    private static Config instance = null;
 
-    private static SingletonConfig instance = null;
-
-    private String storageKey;
-    private Map<String, String> defaultValues;
-    private Map<String, String> masks;
-    private List<Migrator> migrators;
-
-    @Override
-    public String getStorageKey() {
-        return storageKey;
-    }
-
-    @Override
-    public Map<String, String> getDefaultValues() {
-        return defaultValues;
-    }
-
-    @Override
-    public Map<String, String> getMasks() {
-        return masks;
-    }
-
-    @Override
-    public List<Migrator> getMigrators() {
-        return migrators;
-    }
-
-    public void setStorageKey(String storageKey) {
-        this.storageKey = storageKey;
-    }
-
-    public void setDefaultValues(Map<String, String> defaultValues) {
-        this.defaultValues = defaultValues;
-    }
-
-    public void setMasks(Map<String, String> masks) {
-        this.masks = masks;
-    }
-
-    public void setMigrators(List<Migrator> migrators) {
-        this.migrators = migrators;
-    }
-
-    public static SingletonConfig getInstance() {
+    public static Config getInstance() {
         if(instance == null) {
-            instance = new SingletonConfig();
+            instance = new Config();
         }
         return instance;
     }
--- src/main/java/de/scandio/settingsframework/settings/Config.java
+++ src/main/java/de/scandio/settingsframework/settings/Config.java
@@ -1,15 +1,67 @@
 package de.scandio.settingsframework.settings;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public interface Config {
+public class Config {
 
-    String getStorageKey();
+    private String storageKey;
+    private Map<String, String> defaultValues;
+    private Map<String, String> masks;
+    private List<Migrator> migrators;
 
-    Map<String, String> getDefaultValues();
+    public Config() {
+    }
 
-    Map<String, String> getMasks();
+    public Config(String storageKey, Map<String, String> defaultValues, Map<String, String> masks, List<Migrator> migrators) {
+        this.storageKey = storageKey;
+        this.defaultValues = defaultValues;
+        this.masks = masks;
+        this.migrators = migrators;
+    }
 
-    List<Migrator> getMigrators();
+    public String getStorageKey() {
+        return storageKey;
+    }
+
+    public Map<String, String> getDefaultValues() {
+        return defaultValues;
+    }
+
+    public Map<String, String> getMasks() {
+        return masks;
+    }
+
+    public List<Migrator> getMigrators() {
+        return migrators;
+    }
+
+    public Config setStorageKey(String storageKey) {
+        this.storageKey = storageKey;
+        return this;
+    }
+
+    public Config setDefaultValues(Map<String, String> defaultValues) {
+        this.defaultValues = defaultValues;
+        return this;
+    }
+
+    public Config setMasks(Map<String, String> masks) {
+        this.masks = masks;
+        return this;
+    }
+
+    public Config setMigrators(List<Migrator> migrators) {
+        this.migrators = migrators;
+        return this;
+    }
+
+    public Config addMigrator(Migrator migrator) {
+        if (this.migrators == null) {
+            this.migrators = new ArrayList<>();
+        }
+        this.migrators.add(migrator);
+        return this;
+    }
 }
