aa661b18cdac0efc332640d308e1e6ccb27834bd
mroehrs <martin.roehrs@scandio.de>
2019-03-06 12:25:31 +0100

    Fix the jquery id selector possibly breaking on special characters (escapeSelector method copied from jQuery 3.0)

--- src/main/resources/settings-framework/js/settings.js
+++ src/main/resources/settings-framework/js/settings.js
@@ -38,9 +38,32 @@
         });
       };
 
+      var escapeSelector = $.escapeSelector || function (selector) {
+        // CSS string/identifier serialization
+        // https://drafts.csswg.org/cssom/#common-serializing-idioms
+        var rcssescape = /([\0-\x1f\x7f]|^-?\d)|^-$|[^\x80-\uFFFF\w-]/g;
+        var fcssescape = function (ch, asCodePoint) {
+          if (asCodePoint) {
+
+            // U+0000 NULL becomes U+FFFD REPLACEMENT CHARACTER
+            if (ch === "\0") {
+              return "\uFFFD";
+            }
+
+            // Control characters and (dependent upon position) numbers get escaped as code points
+            return ch.slice(0, -1) + "\\" + ch.charCodeAt(ch.length - 1).toString(16) + " ";
+          }
+
+          // Other potentially-special ASCII characters get backslash-escaped
+          return "\\" + ch;
+        };
+        return (selector + "").replace(rcssescape, fcssescape);
+      };
+
       var fillValuesIntoForm = function(values) {
         Object.keys(values).forEach(function(key) {
-          var $input = $('#' + key);
+          var id = escapeSelector(key);
+          var $input = $('#' + id);
           if ($input.is(':checkbox')) {
             if (values[key] === 'true') {
               $input.attr('checked', true);
